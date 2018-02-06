c31af0eac9fbea2005082b6fbc89e0e8bf5544a4
gioboa <giorgiob.boa@gmail.com>
2018-02-06 18:08:10 +0100

    add project files

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor(private swUpdate: SwUpdate) {}
+
+  ngOnInit() {
+      if (this.swUpdate.isEnabled) {
+          this.swUpdate.available.subscribe(() => {
+              if(confirm("New version available. Load New Version?")) {
+                  window.location.reload();
+              }
+          });
+      }        
+  }
+}
